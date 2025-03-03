6c70ca1e2c287271ca3fb9ccbcdf733518e4e281
chanuajohnson <chanuajohnson@gmail.com>
2025-03-03 12:58:35 -0400

    Create LoadingScreen.tsx

---
+++ src/components/ui/LoadingScreen.tsx
@@ -0,0 +1,16 @@
+const LoadingScreen = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '1.5rem',
+      fontWeight: 'bold'
+    }}>
+      <h2>Loading...</h2>
+    </div>
+  );
+};
+
+export default LoadingScreen;
