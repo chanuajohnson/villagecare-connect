c99ddf5dd65ac9424f4ad4b7f704f3ec38ad01ea
chanuajohnson <chanuajohnson@gmail.com>
2025-03-03 22:41:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import ProfessionalFeaturesOverview from "./pages/professional/ProfessionalFeatu
 import FamilyFeaturesOverview from "./pages/family/FamilyFeaturesOverview";
 import FAQPage from "./pages/support/FAQPage";
 import { Fab } from "@/components/ui/fab";
+import "./debugEnv";
 
 const queryClient = new QueryClient();
 
